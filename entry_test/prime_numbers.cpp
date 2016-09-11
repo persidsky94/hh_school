97f46f6e524a90f0d56483ec434ee890b79b5fa4
persidsky94 <persidsky94@gmail.com>
2016-09-11 22:47:29 +0300

    Minor 5 problem changes

--- entry_test/prime_numbers.cpp
+++ entry_test/prime_numbers.cpp
@@ -3,4 +3,16 @@
 #include <cmath>
 using namespace std;
 
+int initial_size=1;
+int top_limit=10000;
+int limiter=int(sqrt(top_limit));
 
+int main() 
+{
+    vector<vector<int>> a;
+				int i=0;
+    while (i < limiter)
+				{
+								
+				}
+}
